6e4c79957639632099f4ee9d766a5fd5321337ce
Carlos <alfredomontoyagit@gmail.com>
2022-07-27 09:14:55 -0500

    Inivi Graficas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+import React, { useEffect, useState } from 'react';
+
+
 
 function App() {
+  
+  const [moneda, setMoneda] = useState([]);
+
+
+  useEffect(() => {
+
+    const consultaApi = async () => {
+
+
+      const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=5&tsym=USD";
+
+      const resp = await fetch(url)
+      const resultado = await resp.json();
+       
+      const arraMaxCripto = resultado.Data.map((critos) => {
+          
+        const cripto ={
+          id:critos.CoinInfo.Id,
+          nombre:critos.CoinInfo.FullName,
+          maxValor:critos.CoinInfo.MaxSupply,
+
+        }
+        return cripto
+      })
+      setMoneda(arraMaxCripto);
+    }
+    consultaApi();
+    
+  }, [])
+  console.log(moneda);
+  
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+       <h1>hola</h1>
     </div>
+    </>
   );
 }
 
